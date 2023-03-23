3001516b7bfd5f9f0af582e7a33c2000da8412e6
PaolaBramlett <paolabramlett@gmail.com>
2023-03-23 11:26:50 -0600

    Paola

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import './style.css';
+//import './styles.css';
 
 const Navbar = () => {
     return (
         <div>
-            <nav>
-                
+            <nav className='d-flex'>
+                <div className='container'>
+                    <img src='' alt='logo'/>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" className='form-control' placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button">Button</button>
+                    </div>
+                </div>
             </nav>
         </div>
     )

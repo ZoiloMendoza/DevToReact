d53c1183b8e77744df67d7e4567932edd93aa12c
ZoiloMendoza <zoilomm@hotmail.com>
2023-03-23 17:32:10 -0600

    App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import AsideRight from './components/Aside-right/Aside-right';
 import Footer from './components/Footer/Footer';
 //import Navbar from './components/Navbar/Navbar';
 import Asideleft from './components/Asideleft/Asideleft'

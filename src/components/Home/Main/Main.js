a49d3e899deb8fd0be062963b9fc4f7a6dc99eac
PaolaBramlett <paolabramlett@gmail.com>
2023-03-24 11:51:52 -0600

    paola

--- src/components/Home/Main/Main.js
+++ src/components/Home/Main/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AsideRight from '../../Aside-right/Aside-right'
 //import AsideLeftZ from "../../Asideleft/AsideLeftZ"
-import AsideLeft from "../../AsideLeft/AsideLeft";
+import AsideLeft from '../../AsideLeft/AsideLeft';
 
 
 const Main = () =>{
@@ -10,7 +10,7 @@ const Main = () =>{
         <main>
             <selection className='page-container container-xl d-flex justify-content-between'>
                 <selection className='page__leftColumn'>{/**Columna 1 */}
-                    <AsideLeft/>
+                    <AsideLeft />
                 </selection>
                 <selection className='page__centerColumn'>{/**Columna 2 */}
 

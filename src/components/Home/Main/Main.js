c99d57b3f41043cd359ae2460080a71d13001ddd
ZoiloMendoza <zoilomm@hotmail.com>
2023-03-23 21:56:34 -0600

    Z agregando estrutura a main

--- src/components/Home/Main/Main.js
+++ src/components/Home/Main/Main.js
@@ -1,11 +1,24 @@
-import AsideRight from "../../Aside-right/Aside-right";
+import React from "react";
+import AsideRight from '../../Aside-right/Aside-right'
+import AsideLeft from "../../Asideleft/Asideleft"
+import '../styles.scss'
 
 const Main = () =>{
 
     return (
-        <>
-        <AsideRight/>
-        </>
+        <main>
+            <selection className='page-container container-xl d-flex justify-content-between'>
+                <selection className='page__leftColumn'>{/**Columna 1 */}
+                <AsideLeft/>
+                </selection>
+                <selection className='page__centerColumn'>{/**Columna 2 */}
+
+                </selection>
+                <section class="page__rightColumn">{/**Columna 3 */}
+                    <AsideRight/>
+                </section>
+            </selection>
+        </main>
     )
 }
 

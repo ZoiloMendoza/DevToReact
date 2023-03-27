98a7481a2a0c49a4696712d5bc45403755569aba
PaolaBramlett <paolabramlett@gmail.com>
2023-03-27 13:08:23 -0600

    Paola

---
+++ src/components/Login/Navbar/userLogin.js
@@ -0,0 +1,16 @@
+import Navbar from "../../Navbar/Navbar";
+import Footer from "../../Footer/Footer";
+
+const userLogin = () => {
+    return (
+        <body>
+            <Navbar />
+
+            <section>
+
+            </section>
+
+            <Footer />
+        </body>
+    )
+}
